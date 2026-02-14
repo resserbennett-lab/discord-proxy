08ab8928a47aff044873ae003d808144e2bf5b44
resserbennett-lab <resserbennett@gmail.com>
2026-02-13 22:02:10 -0700

    Update index.js

--- discord proxy🤑🤑🤑/index.js
+++ discord proxy🤑🤑🤑/index.js
@@ -1,4 +1,4 @@
-const { Client, GatewayIntentBits } = require("discord.js");
+const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [
@@ -8,15 +8,14 @@ const client = new Client({
   ]
 });
 
-client.once("ready", () => {
+client.once('ready', () => {
   console.log(`Logged in as ${client.user.tag}`);
 });
 
-client.on("messageCreate", message => {
-  if (message.author.bot) return;
-  if (message.content === "!ping") {
-    message.reply("Pong!");
+client.on('messageCreate', message => {
+  if (message.content === '!ping') {
+    message.reply('Pong!');
   }
 });
-client.login(process.env.TOKEN);
 
+client.login(process.env.TOKEN);
